cb5466db0fe13de73e6ad7ee49a9dd78fd76aab7
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2021-12-12 16:25:10 -0500

    feat: scan word on hover

--- src/FactorHoverProvider.ts
+++ src/FactorHoverProvider.ts
@@ -7,15 +7,25 @@ import {
   TextDocument,
 } from 'vscode'
 
+const factorWordRegex = /\S+/
+
 export default class FactorHoverProvider implements HoverProvider {
   provideHover(
     document: TextDocument,
     position: Position,
     token: CancellationToken,
   ): ProviderResult<Hover> {
-    console.log('hover!', document, position, token)
+    // Need to look up the definition in the context of the vocab and its usings.
+    // A FIXME to updating the usings if it's outside the current ones would be great.
+    const wordRange = document.getWordRangeAtPosition(position, factorWordRegex)
+    if (!wordRange) {
+      return
+    }
+
+    const word = document.getText(wordRange)
     const placeholder = new Hover(
-      'One day, this will tell you something useful.',
+      `One day, this will tell you something useful about \`${word}\`.`,
+      wordRange,
     )
     return placeholder
   }
